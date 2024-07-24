9a15dbff7bd65990b4b1f9a47f4b94a0b5e91187
Muhammad Sarim Hassan <shassan@EXARTA.local>
2024-07-24 17:31:49 +0500

    added shadows

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,9 +1,9 @@
-import { Center, Environment, Float, useScroll } from '@react-three/drei';
+import { Center, Environment, Float, useScroll, ContactShadows } from '@react-three/drei';
 import { MotionConfig } from 'framer-motion';
 import { motion } from 'framer-motion-3d';
 
 import { Avatar, SectionTitle } from '../../components';
-import { useRef, useState } from 'react';
+import { Suspense, useRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 import config from '../../config';
 
@@ -21,90 +21,105 @@ const Home = () => {
   });
 
   return (
-    <MotionConfig
-      transition={{
-        duration: 0.6,
-      }}
-    >
-      <group position-y={-1}>
-        <Environment preset='sunset' />
-        <Avatar />
-        <motion.group ref={sceneContainer} animate={section}>
-          {/* Home */}
-          <motion.group
-            position-y={10}
-            variants={{
-              home: {
-                y: 0,
-              },
-            }}
-          >
-            <Center disableY disableZ>
-              <SectionTitle
-                size={0.6}
-                position-x={-1}
-                rotation={[0, 0.5, 0]}
-                position-y={1.2}
-                position-z={0}
-                bevelEnabled
-                bevelThickness={0.1}
-              >
-                Sarim
-              </SectionTitle>
-            </Center>
+    <Suspense>
+      <MotionConfig
+        transition={{
+          duration: 0.6,
+        }}
+      >
+        <group position-y={-1}>
+          {/* Light for the models  */}
+          <Environment preset='sunset' />
 
-            <Center disableY disableZ>
-              <SectionTitle
-                rotation={[0, 0.5, 0]}
-                position-x={-2}
-                size={1}
-                position-y={0}
-                position-z={-1}
-                bevelEnabled
-                bevelThickness={0.1}
-              >
-                Hassan
-              </SectionTitle>
-            </Center>
-          </motion.group>
+          {/* Avatar Model */}
+          <Avatar />
 
-          {/* Skills */}
-          <motion.group
-            position-z={Section_Distance}
-            position-y={10}
-            variants={{
-              skills: {
-                y: 0,
-              },
-            }}
-          >
-            <SectionTitle position-x={0.5}>SKILLS</SectionTitle>
-          </motion.group>
+          {/* Shadow for the name  */}
+          <ContactShadows opacity={0.5} scale={[30, 30]} color='#9c8e66' />
 
-          {/* Projects */}
-          <motion.group
-            position-y={10}
-            variants={{
-              projects: {
-                y: 0,
-              },
-            }}
-            position-z={Section_Distance * 2}
-          >
-            <SectionTitle position-x={0.5}>PROJECTS</SectionTitle>
-          </motion.group>
+          {/* Platform Shade */}
+          <mesh position-y={-0.001} rotation-x={-Math.PI / 2}>
+            <planeGeometry args={[100, 100]} />
+            <meshBasicMaterial color='#f5f3ee' />
+          </mesh>
 
-          {/* Contact */}
-          <motion.group
-            position-y={10}
-            variants={{ contact: { y: 0 } }}
-            position-z={Section_Distance * 3}
-          >
-            <SectionTitle position-x={0.5}>CONTACT</SectionTitle>
+          <motion.group ref={sceneContainer} animate={section}>
+            {/* Home */}
+            <motion.group
+              position-y={10}
+              variants={{
+                home: {
+                  y: 0,
+                },
+              }}
+            >
+              <Center disableY disableZ>
+                <SectionTitle
+                  size={0.6}
+                  position-x={-1}
+                  rotation={[0, 0.5, 0]}
+                  position-y={1.2}
+                  position-z={0}
+                  bevelEnabled
+                  bevelThickness={0.1}
+                >
+                  Sarim
+                </SectionTitle>
+              </Center>
+
+              <Center disableY disableZ>
+                <SectionTitle
+                  rotation={[0, 0.5, 0]}
+                  position-x={-2}
+                  size={1}
+                  position-y={0}
+                  position-z={-1}
+                  bevelEnabled
+                  bevelThickness={0.1}
+                >
+                  Hassan
+                </SectionTitle>
+              </Center>
+            </motion.group>
+
+            {/* Skills */}
+            <motion.group
+              position-z={Section_Distance}
+              position-y={10}
+              variants={{
+                skills: {
+                  y: 0,
+                },
+              }}
+            >
+              <SectionTitle position-x={0.5}>SKILLS</SectionTitle>
+            </motion.group>
+
+            {/* Projects */}
+            <motion.group
+              position-y={10}
+              variants={{
+                projects: {
+                  y: 0,
+                },
+              }}
+              position-z={Section_Distance * 2}
+            >
+              <SectionTitle position-x={0.5}>PROJECTS</SectionTitle>
+            </motion.group>
+
+            {/* Contact */}
+            <motion.group
+              position-y={10}
+              variants={{ contact: { y: 0 } }}
+              position-z={Section_Distance * 3}
+            >
+              <SectionTitle position-x={0.5}>CONTACT</SectionTitle>
+            </motion.group>
           </motion.group>
-        </motion.group>
-      </group>
-    </MotionConfig>
+        </group>
+      </MotionConfig>
+    </Suspense>
   );
 };
 
